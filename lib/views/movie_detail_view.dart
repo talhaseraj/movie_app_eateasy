a6ce61e01196d17a5ba63415cc66be7528b72271
talha seraj <talhaseraj92@gmail.com>
2024-12-19 14:52:09 +0400

    update

--- lib/views/movie_detail_view.dart
+++ lib/views/movie_detail_view.dart
@@ -166,7 +166,7 @@ class MovieDetailView extends StatelessWidget {
                               ),
                             );
                           }),
-                    )
+                    ),
                 ],
               ),
             );
--- lib/views/movie_list_view.dart
+++ lib/views/movie_list_view.dart
@@ -46,38 +46,158 @@ class MovieListView extends StatelessWidget {
           if (_.isLoading && _.movies.isEmpty) {
             return Center(child: CircularProgressIndicator());
           }
-          return ListView(
-            controller: _.scrollController,
-            children: List.from(
-              _.movies.map(
-                (movie) => ListTile(
-                  title: Text(movie.title ?? ""),
-                  subtitle: Text('Rating: ${movie.voteAverage}'),
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Hero(
-                      tag: movie.id.toString(),
-                      child: CachedNetworkImage(
-                        imageUrl:
-                            'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                        progressIndicatorBuilder: (ctx, str, indicator) {
-                          return CupertinoActivityIndicator();
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Favorite Movies",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                    padding: EdgeInsets.only(left: 10),
+                    shrinkWrap: true,
+                    itemCount: _.favoriteMovieIds.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final movie = _.movies
+                          .firstWhere((e) => e.id == _.favoriteMovieIds[index]);
+                      return SizedBox(
+                        width: 100,
+                        height: 200,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 140,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: CachedNetworkImage(
+                                  imageUrl:
+                                      'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                                ),
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              movie.title.toString(),
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "(${movie.releaseDate!.year})",
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              Divider(),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Watchlist Movies",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                    padding: EdgeInsets.only(left: 10),
+                    shrinkWrap: true,
+                    itemCount: _.watchlistMovieIds.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final movie = _.movies.firstWhere(
+                          (e) => e.id == _.watchlistMovieIds[index]);
+                      return SizedBox(
+                        width: 100,
+                        height: 200,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 140,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: CachedNetworkImage(
+                                  imageUrl:
+                                      'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                                ),
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              movie.title.toString(),
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              "(${movie.releaseDate!.year})",
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              Divider(),
+              Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  controller: _.scrollController,
+                  children: List.from(
+                    _.movies.map(
+                      (movie) => ListTile(
+                        title: Text(movie.title ?? ""),
+                        subtitle: Text('Rating: ${movie.voteAverage}'),
+                        leading: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Hero(
+                            tag: movie.id.toString(),
+                            child: CachedNetworkImage(
+                              imageUrl:
+                                  'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                              progressIndicatorBuilder: (ctx, str, indicator) {
+                                return CupertinoActivityIndicator();
+                              },
+                            ),
+                          ),
+                        ),
+                        onTap: () {
+                          Helpers.addLoadingOverlay(context);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  MovieDetailView(movie: movie),
+                            ),
+                          );
                         },
                       ),
                     ),
                   ),
-                  onTap: () {
-                    Helpers.addLoadingOverlay(context);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MovieDetailView(movie: movie),
-                      ),
-                    );
-                  },
                 ),
               ),
-            ),
+            ],
           );
         },
       ),
